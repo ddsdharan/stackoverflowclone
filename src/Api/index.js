f63ec2e234479fc40bc087e3167bc9573aa2f6f5
ddsdharan <128017667+ddsdharan@users.noreply.github.com>
2023-06-20 02:09:02 +0530

    Update index.js

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: " http://localhost:8000" })
+const API = axios.create({ baseURL: "https://clone-stackoverflow-1tqs.onrender.com" })
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
